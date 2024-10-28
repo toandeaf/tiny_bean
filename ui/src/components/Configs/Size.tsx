ddec9e6dc4c40be374067e09f4f443c3ea072e02
jaketoan <jaketoan@hotmail.co.uk>
2024-10-28 13:43:27 +0000

    Styling updates

--- ui/src/components/Configs/Size.tsx
+++ ui/src/components/Configs/Size.tsx
@@ -3,36 +3,23 @@ import { useState } from 'react';
 
 type LocalSize = 'S' | 'M' | 'L';
 
+const SIZES: Array<LocalSize> = ['S', 'M', 'L'];
 const Size = () => {
   const [selected, setSelected] = useState<LocalSize>('M');
 
   // TODO - replace these wee circles with coffee cups with letters?
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-      }}
-    >
-      <button
-        className={`size-button ${selected === 'S' ? 'selected' : ''}`}
-        onClick={() => setSelected('S')}
-      >
-        S
-      </button>
-      <button
-        className={`size-button ${selected === 'M' ? 'selected' : ''}`}
-        onClick={() => setSelected('M')}
-      >
-        M
-      </button>
-      <button
-        className={`size-button ${selected === 'L' ? 'selected' : ''}`}
-        onClick={() => setSelected('L')}
-      >
-        L
-      </button>
+    <div className={'size-container'}>
+      {SIZES.map((size) => {
+        return (
+          <button
+            className={`size-button ${selected === size ? 'selected' : ''}`}
+            onClick={() => setSelected(size)}
+          >
+            {size}
+          </button>
+        );
+      })}
     </div>
   );
 };
