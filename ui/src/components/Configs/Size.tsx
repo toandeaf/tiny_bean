acf35bb6b9ee4818816d0e888a998abda34b2e00
jaketoan <jaketoan@hotmail.co.uk>
2024-10-28 13:31:43 +0000

    Size functionality

--- ui/src/components/Configs/Size.tsx
+++ ui/src/components/Configs/Size.tsx
@@ -1,6 +1,11 @@
 import './Configs.css';
+import { useState } from 'react';
+
+type LocalSize = 'S' | 'M' | 'L';
 
 const Size = () => {
+  const [selected, setSelected] = useState<LocalSize>('M');
+
   // TODO - replace these wee circles with coffee cups with letters?
   return (
     <div
@@ -10,9 +15,24 @@ const Size = () => {
         justifyContent: 'space-between',
       }}
     >
-      <button className="size-button">S</button>
-      <button className="size-button selected">M</button>
-      <button className="size-button">L</button>
+      <button
+        className={`size-button ${selected === 'S' ? 'selected' : ''}`}
+        onClick={() => setSelected('S')}
+      >
+        S
+      </button>
+      <button
+        className={`size-button ${selected === 'M' ? 'selected' : ''}`}
+        onClick={() => setSelected('M')}
+      >
+        M
+      </button>
+      <button
+        className={`size-button ${selected === 'L' ? 'selected' : ''}`}
+        onClick={() => setSelected('L')}
+      >
+        L
+      </button>
     </div>
   );
 };
