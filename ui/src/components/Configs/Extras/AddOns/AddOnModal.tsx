ae37f110f906494f61bad94a95c7ece9d85547ea
jaketoan <jaketoan@hotmail.co.uk>
2024-10-29 19:26:37 +0000

    Quick tidy

--- ui/src/components/Configs/Extras/AddOns/AddOnModal.tsx
+++ ui/src/components/Configs/Extras/AddOns/AddOnModal.tsx
@@ -1,8 +1,8 @@
-import ActionButtons from '../../../ActionButtons.tsx';
 import Modal from '../../../Modal/Modal.tsx';
 import { FC } from 'react';
 import { EXTRAS, SYRUPS } from '../../../../data/addOnOptions.ts';
 import AddOnSection from './AddOnSection.tsx';
+import ModalButtons from '../../../Modal/ModalButtons.tsx';
 
 interface Props {
   isModalOpen: boolean;
@@ -19,20 +19,12 @@ const AddOnModal: FC<Props> = ({ isModalOpen, setIsModalOpen }) => {
       <AddOnSection title={'Syrups'} options={SYRUPS} />
       <AddOnSection title={'Extras'} options={EXTRAS} />
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          paddingTop: '0.5em',
-        }}
-      >
-        <ActionButtons
-          primaryText={'Confirm add-ons'}
-          primaryAction={() => {}}
-          cancelText={'Clear'}
-          cancelAction={() => {}}
-        />
-      </div>
+      <ModalButtons
+        primaryText={'Confirm add-ons'}
+        primaryAction={() => {}}
+        cancelText={'Clear'}
+        cancelAction={() => {}}
+      />
     </Modal>
   );
 };
