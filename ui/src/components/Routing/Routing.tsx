929538eaf70fc1549ef3f8221834033b2620ce25
jaketoan <jaketoan@hotmail.co.uk>
2024-11-27 13:00:43 +0000

    Example for Sambo

--- ui/src/components/Routing/Routing.tsx
+++ ui/src/components/Routing/Routing.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LandingPage from 'pages/LandingPage/LandingPage.tsx'
 import OrderPlacePage from 'pages/OrderPlacePage/OrderPlacePage.tsx'
 import OrderViewPage from 'pages/OrderViewPage/OrderViewPage.tsx'
 
 const Routing = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/orders/place" element={<OrderPlacePage />} />
-      <Route path="/orders/view" element={<OrderViewPage />} />
-    </Routes>
-  </Router>
+    <Router>
+        <Routes>
+            <Route path={"/"} element={<LandingPage/>}/>
+            <Route path="/" element={<LandingPage/>}/>
+            <Route path="/orders/place" element={<OrderPlacePage/>}/>
+            <Route path="/orders/view" element={<OrderViewPage/>}/>
+        </Routes>
+    </Router>
 )
 
 export default Routing
